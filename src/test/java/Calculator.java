e1433da55cfc9729a5e7d5f797358da501688f44
Paresh Wardhan <Paresh.wardhan@gmail.com>
2022-03-15 15:14:29 +0530

    Added the test shouldAddTwoNumbers and shouldMultiplyTwoNumber and added Calculator class

---
+++ src/test/java/Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    public int getSum(int number1, int number2) {
+        return number1 + number2;
+    }
+
+    public int getResult(int number1, int number2) {
+        return number1 * number2;
+    }
+}
--- src/test/java/StringUtilTest.java
+++ src/test/java/StringUtilTest.java
@@ -8,7 +8,23 @@ public class StringUtilTest {
 
         String teststring="Hello World!";
         Assert.assertEquals(teststring.length(),12);
-
     }
+
+    @Test
+    public void shouldAddTwoNumbers(){
+        int sum = new Calculator().getSum(10, 20);
+        Assert.assertEquals(sum,30);
+    }
+
+
+
+    @Test
+    public void shouldMultiplyTwoNumbers(){
+        int result =new Calculator().getResult(20, 10);
+        Assert.assertEquals(result,200);
+    }
+
+
+
 
 }
